ca935994213b36b13fc79d8693c0079c61427c54
mahbeenara <79276061+mahbeenara@users.noreply.github.com>
2021-02-19 01:00:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.review-content').owlCarousel({
+		items: 1,
+		autoPlay: true,
+		stopOnHover: true,
+	});
+	$('.logos').owlCarousel({
+		pagination: false,
+	});
+});
